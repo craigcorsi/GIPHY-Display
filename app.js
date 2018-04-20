03ae818afc563c6fca701ce6a25db8431b26a245
Craig <corsi007@umn.edu>
2018-04-19 20:08:30 -0500

    dynamically populate with buttons from an array

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 // My personal GIPHY API key
 var apiKey = 'irKsuYqDdx0NOhuarY8zv12EF5Jp48jk';
 
-// Function to take an array of GIPHY image data and display the images in browser.
+// array of search topics, dynamically appended
+var topics = ['burgers', 'pastrami', 'hoagie'];
 
-function parseImages(data) {
+// add a button with a search term to the DOM
+function addButton (searchTerm) {
+    var newButton = $('<button>').attr({'data-name': searchTerm, 'class': 'term-select'}).text(searchTerm);
+    $('#buttonsHere').append(newButton);
+}
+
+// display all buttons whose labels are listed in an array
+function displayButtons(topics) {
+    $('#buttonsHere').html('');
+    for (i=0; i < topics.length; i++) {
+        var searchTerm = topics[i];
+        addButton(searchTerm);
+    }
+}
+
+// take an array of GIPHY image data and display the images in browser.
+
+function displayImages(data) {
     // clear image container of previous images
 
-    console.log(data);
-
     l = data.length;
-    for (var i=0; i < l; i++) {
+    for (var i = 0; i < l; i++) {
         var url = data[i].images.fixed_height.url;
         var url_still = data[i].images.fixed_height_still.url;
         var image = $('<img>').attr({
@@ -19,38 +35,44 @@ function parseImages(data) {
             'data-animated': url
         });
         $('#imagesHere').append(image);
-        console.log(image);
     }
-    
 }
 
+function retrieveData(searchTerm) {
+    console.log(searchTerm);
+    $('#imagesHere').html('');
+    var queryURL = "https://api.giphy.com/v1/gifs/search?apikey=" + apiKey + "&q=" + searchTerm + "&limit=10&lang=en";
 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        var data = response.data;
+        displayImages(data);
+    });
+}
 
+function toggleImage(img) {
+    if (img.attr('data-state') == 'still') {
+        img.attr('src', img.attr('data-animated')).attr('data-state', 'animated');
+    } else {
+        img.attr('src', img.attr('data-still')).attr('data-state', 'still');
+    }
+}
 
-// search term string obtained from user input
-var searchTerm = "burgers";
-var queryURL = "https://api.giphy.com/v1/gifs/search?apikey=" + apiKey + "&q=" + searchTerm + "&limit=10&lang=en";
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-    var data = response.data;
-    parseImages(data);
+$('body').on('click', 'img', function () {
+    toggleImage($(this));
 });
 
+$('body').on('click', '.term-select', function () {
+    var searchTerm = $(this).attr('data-name');
+    retrieveData(searchTerm);
+});
 
+$('body').on('click', '#buttonSubmit', function () {
+    var searchTerm = $('#addTerm').val().trim();
+    topics.push(searchTerm);
+    displayButtons(topics);
+});
 
-//   // Get reference to existing tbody element, create a new table row element
-//   var tBody = $("tbody");
-//   var tRow = $("<tr>");
-
-//   // Methods run on jQuery selectors return the selector they we run on
-//   // This is why we can create and save a reference to a td in the same statement we update its text
-//   var titleTd = $("<td>").text(response.Title);
-//   var yearTd = $("<td>").text(response.Year);
-//   var actorsTd = $("<td>").text(response.Actors);
-//   // Append the newly created table data to the table row
-//   tRow.append(titleTd, yearTd, actorsTd);
-//   // Append the table row to the table body
-//   tBody.append(tRow);
+displayButtons(topics);
